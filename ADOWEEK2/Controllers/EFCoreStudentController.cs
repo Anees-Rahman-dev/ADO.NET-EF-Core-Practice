104abf09f511646acce1bd9c90552564e9836f27
Anees-Rahman-dev <aneesrahman0101@gmail.com>
2026-09-10 14:29:19 +0530

    Added GetAllStudentsEF

--- ADOWEEK2/Controllers/EFCoreStudentController.cs
+++ ADOWEEK2/Controllers/EFCoreStudentController.cs
@@ -1,6 +1,37 @@
-﻿namespace ADOWEEK2.Controllers
+﻿using ADOWEEK2.Data;
+using Microsoft.AspNetCore.Mvc;
+using ADOWEEK2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ADOWEEK2.Controllers
 {
-    public class Student
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EFCoreStudentController : ControllerBase
     {
+        private readonly ApplicationDbContext _context;
+
+        public EFCoreStudentController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+
+        public async Task<IActionResult> GetAllStudents()
+        {
+            var students = await _context.Students.ToListAsync();
+            return Ok(students);
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddStudents(Student student)
+        {
+            _context.Students.Add(student);
+
+            await _context.SaveChangesAsync();
+
+            return Ok(student);
+        }
+
     }
 }
