5ad850846618f1caf5c9dce96de777867a23f38b
Anees-Rahman-dev <aneesrahman0101@gmail.com>
2026-09-10 15:19:56 +0530

    Added UpdateSet

--- ADOWEEK2/Controllers/EFCoreStudentController.cs
+++ ADOWEEK2/Controllers/EFCoreStudentController.cs
@@ -20,7 +20,7 @@ namespace ADOWEEK2.Controllers
 
         public async Task<IActionResult> GetAllStudents()
         {
-            var students = await _context.Students.ToListAsync();
+            var students = await _context.Students.ToListAsync(); // It executes a database query and puts the results into a List<T> asynchronously
             return Ok(students);
         }
         [HttpPost]
@@ -28,9 +28,29 @@ namespace ADOWEEK2.Controllers
         {
             _context.Students.Add(student);
 
-            await _context.SaveChangesAsync();
+            await _context.SaveChangesAsync();//Asynchronously saves all changes made in this context to the underlying database.
 
             return Ok(student);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateStudent(int id, Student student)
+        {
+            var IsStudentExist = await _context.Students.FindAsync(id);
+
+            if (IsStudentExist == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                IsStudentExist.name = student.name;
+                IsStudentExist.Age = student.Age;
+            }
+
+            await _context.SaveChangesAsync();
+            return Ok(IsStudentExist);
+
         }
 
     }
